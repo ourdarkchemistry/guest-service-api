fc0a7954945c854001ad10ec1da72d39c0fc3dee
ourdarkchemistry <ourdarkchemistry@gmail.com>
2024-11-16 21:19:10 +0400

    Create settings.php

---
+++ src/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'settings' => [
+        'displayErrorDetails' => true,
+        'db' => [
+            'host' => 'db',
+            'dbname' => 'guest_service',
+            'user' => 'root',
+            'pass' => 'root'
+        ]
+    ]
+];
